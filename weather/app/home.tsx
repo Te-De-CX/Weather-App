45bc9d7d4a55de841f151b5207e8cb5574da1501
teslim <tunjiheritage@gmail.com>
2025-03-22 01:01:47 +0100

    first commit

---
+++ weather/app/home.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const home = () => {
+  return (
+    <View>
+      <Text className='text-white' >Hello home</Text>
+    </View>
+  )
+}
+
+export default home
